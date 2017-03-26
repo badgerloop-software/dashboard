c85b566670f0d870bd1f030aaf1b7a2f692a2ea6
Administrator <admin@MacBook-Pro-5.local>
2017-03-25 20:16:52 -0500

    Added basic structure to Angular2 app Needed  - Adding pages for each team 	- add component for each metric we are tracking (speed, temp,ect) Structure 	app.component 		-team page 			|_metrics(teams specify which metic they want on their dashboard home)

--- gitDash/src/app/app.module.ts
+++ gitDash/src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SpeedComponent } from './speed/speed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    SpeedComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
