83b862852af3717d54166a1ec3227980f8deb4d8
Vaughn Kottler <vkottler@wisc.edu>
2017-07-28 22:46:06 -0500

    baby steps for API

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"log"
-	//"net/http"
+	"net/http"
 	"fmt"
 
 	"github.com/badgerloop-software/dashboard/database"
-	//api "github.com/badgerloop-software/dashboard/services"
+	api "github.com/badgerloop-software/dashboard/services"
 	models "github.com/badgerloop-software/dashboard/models"
-	//restful "github.com/emicklei/go-restful"
+	restful "github.com/emicklei/go-restful"
 )
 
-/*
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Path accessed: %s", r.URL.Path[1:])
+
+	var err error
+	testData := []models.Data{}
+
+	err = database.GetConnection().Select(&testData, "SELECT * FROM Data LIMIT 1")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Fprintf(w, "(%s) %#v", r.URL.Path[1:], testData)
 }
-*/
 
 func main() {
 
@@ -32,11 +39,12 @@ func main() {
 	}
 
 	fmt.Printf("query returned %d results.\n", len(testData))
+	fmt.Printf("Example: %#v\n", testData[0])
 
 	// Add the api data service
-	//restful.Add(api.New())
+	restful.Add(api.New())
 
 	// Serve on port 2000
-	//http.HandleFunc("/", handler)
-	//log.Fatal(http.ListenAndServe(":2000", nil))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":2000", nil))
 }
