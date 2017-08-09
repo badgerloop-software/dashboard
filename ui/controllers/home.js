5d0a3ca94359ac372d898f7b84e80ef8e6718cfc
vkottler <vkottler@wisc.edu>
2017-08-09 17:04:47 -0500

    working on UI functionality

--- ui/controllers/home.js
+++ ui/controllers/home.js
@@ -1,5 +1,22 @@
+function messageSuccessCallback(response) {
+	console.log("success");
+}
+
+function messageErrorCallback(response) {
+	console.log("error");
+	console.log(response);
+}
+
 angular.module('controllers')
-.controller('homeController', function($scope) {
-	$scope.test = "Hello!";
+.controller('homeController', function($scope, $http) {
+	$scope.sendCommand = function() {
+		console.log("Sending: " + $scope.command);
+		$scope.command = "";
+		// TODO: get this to work
+		$http({
+			method: 'GET',
+			url: 'http://localhost:2000/message?data=hey'
+		}).then(messageSuccessCallback, messageErrorCallback);
+	};
 });
 
