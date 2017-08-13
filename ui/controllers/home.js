13751e27347d6c06dcbe63cdeaa7d5968e819184
Vaughn Kottler <vkottler@wisc.edu>
2017-08-13 15:03:43 -0500

    buttons and functions added for API testing
    Signed-off-by: Vaughn Kottler <vkottler@wisc.edu>

--- ui/controllers/home.js
+++ ui/controllers/home.js
@@ -1,5 +1,6 @@
 function messageSuccessCallback(response) {
 	console.log("success");
+	console.log(response);
 }
 
 function messageErrorCallback(response) {
@@ -9,14 +10,32 @@ function messageErrorCallback(response) {
 
 angular.module('controllers')
 .controller('homeController', function($scope, $http) {
+
 	$scope.sendCommand = function() {
 		console.log("Sending: " + $scope.command);
-		// TODO: get this to work
 		$http({
 			method: 'GET',
 			url: 'http://localhost:2000/message?data=' + $scope.command
 		}).then(messageSuccessCallback, messageErrorCallback);
 		$scope.command = "";
+	};
+
+	$scope.getData = function() {
+		console.log("getting data");
+		$http({
+			method: 'GET',
+			url: 'http://localhost:2000/buffer'
+		}).then(messageSuccessCallback, messageErrorCallback);
+
+	};
+
+	$scope.resetData = function() {
+		console.log("resetting data");
+		$http({
+			method: 'GET',
+			url: 'http://localhost:2000/buffer?reset'
+		}).then(messageSuccessCallback, messageErrorCallback);
+
 	};
 });
 
