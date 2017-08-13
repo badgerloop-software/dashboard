d06bbfcc4f7b41f66b8ad9d135fb1bba78162291
Vaughn Kottler <vkottler@wisc.edu>
2017-08-13 15:02:33 -0500

    microcontroller output API, fix CORS
    established a byte buffer that exposes collected
    microcontroller output and allow it to be retrieved
    from a browser via GET requests.
    
    Signed-off-by: Vaughn Kottler <vkottler@wisc.edu>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"strings"
+	"bytes"
 	"github.com/badgerloop-software/dashboard/database"
 	//api "github.com/badgerloop-software/dashboard/services"
 	models "github.com/badgerloop-software/dashboard/models"
@@ -24,16 +25,21 @@ func CheckError(err error) {
 /*****************************************************************************/
 var outAddr *net.UDPAddr
 var packet_conn *net.UDPConn
+var mcuBuffer *bytes.Buffer
 
 func initialize_UDP() {
 	var ourAddrP *net.UDPAddr
 	var err error
+
 	ourAddrP, err = net.ResolveUDPAddr("udp", ":3000")
 	CheckError(err)
 	outAddr, err = net.ResolveUDPAddr("udp", "192.168.0.10:3000")
 	CheckError(err)
 	packet_conn, err = net.ListenUDP("udp", ourAddrP)
 	CheckError(err)
+
+	/* initialize empty buffer */
+	mcuBuffer = bytes.NewBuffer(make([]byte, 0))
 }
 
 func UDPServer() {
@@ -56,6 +62,7 @@ func UDPServer() {
 				_, err = packet_conn.WriteToUDP([]byte("new phone who dis"), outAddr)
 			}
 			fmt.Print(string(buf[5:n]))
+			mcuBuffer.Write(buf[:])
 		/* SpaceX Packet */
 		} else if n == 34 {
 			dat, err = models.ParseSpaceXPacket(buf[:34])
@@ -83,35 +90,68 @@ func UDPServer() {
 /*****************************************************************************/
 /*                                HTTP Handlers                              */
 /*****************************************************************************/
+/*
+ * Database Querying API
+ * Purpose: view X latest data points from the database that contains
+ *          microcontroller sensor data
+ * Usage: localhost:2000
+ */
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	var err error
 	testData := []models.Data{}
 
+	// TODO: how many data points do we want?
 	err = database.GetConnection().Select(&testData, "SELECT * FROM Dashboard.Data ORDER BY created DESC LIMIT 1;")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	CheckError(err)
 	myTestData, err := json.Marshal(testData)
-	err2 := json.Unmarshal(myTestData,&testData)
-	if err2 != nil {
-		log.Fatalln(err)
-	}
+	CheckError(err)
+	// why would we need to Marshal and then Unmarshal?
+	err = json.Unmarshal(myTestData,&testData)
+	CheckError(err)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(myTestData)
 }
 
+/*
+ * Microcontroller Command-Forwarding API
+ * Purpose: run commands on microcontroller remotely
+ * Usage: localhost:2000/message?data=<command>
+ */
 func UDPForwardingHandler(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Query()["data"]
 	w.Header().Set("Content-Type", "text/plain; charset=utf8")
-	/* valid API call */
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	/* microcontroller-command API call */
 	if message != nil {
-		//fmt.Println("valid: ", message[0])
+		fmt.Println("valid: ", message[0])
 		w.WriteHeader(http.StatusOK)
 		_, err := packet_conn.WriteToUDP([]byte(message[0]), outAddr)
 		CheckError(err)
 	/* invalid API call */
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+/*
+ * Microcontroller Output Buffer API
+ * Purpose: display console outputs from microcontroller in browser
+ * Usage: localhost:2000/data[?reset]
+ */
+func bufferRequestHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	/* reset buffer */
+	if r.URL.Query()["reset"] != nil {
+		mcuBuffer.Reset()
+		w.WriteHeader(http.StatusOK)
+	/* send buffer contents */
+	} else {
+		w.Write(mcuBuffer.Bytes())
 	}
 }
 /*****************************************************************************/
@@ -140,6 +180,7 @@ func main() {
 	/* Serve on port 2000 */
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/message", UDPForwardingHandler)
+	http.HandleFunc("/buffer", bufferRequestHandler)
 	log.Fatal(http.ListenAndServe(":2000", nil))
 }
 
